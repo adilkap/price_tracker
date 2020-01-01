3fbd37b843012599286215b0099288c381fdb692
adilkap <59031442+adilkap@users.noreply.github.com>
2020-01-01 14:40:48 -0500

    Update track_price.py

--- track_price.py
+++ track_price.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import smtplib
 import time
 
-URL = "https://www.amazon.com/AhaStyle-Silicone-Protective-Keychain-AirPods/dp/B077187F2R/ref=sr_1_3?keywords=airpods+case&qid=1577820252&sr=8-3"
+URL = "https://www.amazon.com/any-product-here"
 
 headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'}
 
@@ -30,21 +30,23 @@ def send_mail():
     server.starttls()
     server.ehlo()
 
-    server.login('adilkapadia0@gmail.com', 'dldjongtbhltoxsm')
+    server.login('emailaddress@gmail.com', 'google-app-password')
 
     subject = 'The price for {0} fell below your price, and is now only {1}!'.format(product_title, product_price)
-    body = 'Click the link below to view the product on Amazon: \n https://www.amazon.com/AhaStyle-Silicone-Protective-Keychain-AirPods/dp/B077187F2R/ref=sr_1_3?keywords=airpods+case&qid=1577820252&sr=8-3'
+    body = 'Click the link below to view the product on Amazon: \n https://www.amazon.com/any-product-here'
     msg =  "Subject: {0} \n\n\n {1}".format(subject, body)
    
 
     server.sendmail(
-        'adilkapadia0@gmail.com', 
-        'kapa2050@mylaurier.ca', 
+        'emailaddress@gmail.com', 
+        'receiving email', 
         msg)
     
     print ("Email has successfuly been sent")
 
     server.quit()
 
-track_price()
+while(True):
+    track_price()
+    time.sleep(86400)
 
